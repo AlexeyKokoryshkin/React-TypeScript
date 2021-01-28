cdb97b4f43756b1a718f6fc12b343377575bd06b
AlexeyKokoryshkin <7f@mail.ru>
2021-01-28 14:52:23 +0300

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-function App() {
+import './styles.css'
+import Navbar from './components/Navbar'
+import TodoPage from './pages/TodoPage'
+import AboutPage from './pages/AboutPage'
+
+const App: React.FC = () => {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Router>
+        <Navbar />
+        <div className="container">
+          <Switch>
+            <Route component={TodoPage} path="/" exact />
+            <Route component={AboutPage} path="/about" />
+          </Switch>
+        </div>
+      </Router>
+    </>
+  ) 
 }
 
 export default App;
